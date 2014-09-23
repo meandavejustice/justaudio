0f61fdd0315df801986008c0972db8492615d0a7
David Adam Justice <david@outthinkgroup.com>
2014-09-22 22:09:35 -0700

    make this usable for files, directories, and add bin

---
+++ README.md
@@ -0,0 +1,37 @@
+# justaudio
+
+<!-- [![NPM](https://nodei.co/npm/justaudio.png?downloads=true)](https://npmjs.org/package/justaudio) -->
+
+## Command Line Usage
+```
+usage: ja {OPTIONS}
+
+  Play audio
+       Accepts: URLs, Directorys, Files
+
+OPTIONS are:
+
+  -h, --help   Display this usage info
+```
+
+# api
+
+* `isURL` arg String: returns boolean
+* `playUrl` arg Path: plays url with data
+* `playLocal` arg Path: plays directory or file
+* `playFile` arg Path: plays file
+* `playDirectory` arg Path: plays directory
+
+# name
+* "justaudio" because it just plays audio.
+* "ja" because ^ & also because [JA](http://its.nycgraffiti.net/tag/ja) is a [p. cool graffiti writer](http://www.graffiti.org/dj/ja.html).
+
+![](http://its.nycgraffiti.net/wp-content/uploads/2009/07/3655959834_1e125b9b0e.jpg)
+
+Here is a picture of him in _state your name_
+![ja in state your name](https://images.duckduckgo.com/iu/?u=https%3A%2F%2Fc1.staticflickr.com%2F1%2F218%2F524271857_58fc621a08_z.jpg%3Fzz%3D1&f=1)
+
+There is also a rumor that he was in the Karate Kid.
+
+# LICENSE
+MIT
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var Speaker = require('speaker');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
 var lame = require('lame');
+var ls = require('ls-stream');
 var request = require('request');
+var Speaker = require('speaker');
 var ProgressBar = require('progress');
 
 var speaker = new Speaker({
@@ -9,29 +13,96 @@ var speaker = new Speaker({
   sampleRate: 44100     // 44,100 Hz sample rate
 });
 
-var url = 'https://api.soundcloud.com/tracks/160842944/download?client_id=b45b1aa10f1ac2941910a7f0d10f8e28&oauth_token=1-16343-36709719-0e51500e5f9c728';
+// console.log(process.argv.slice(2));
 
-module.exports = function() {
-  return
+module.exports = {
+  isURL: isURL,
+  playUrl: playUrl,
+  playLocal: playLocal,
+  playFile: playFile,
+  playDirectory: playDirectory,
+};
+
+function playUrl(url, nodata) {
   request(url)
-  .on('response', function(res){
-    var len = parseInt(res.headers['content-length'], 10);
-    console.log('\n');
+  .on('response', displayData)
+  .pipe(new lame.Decoder())
+  .pipe(speaker);
+}
 
-    var bar = new ProgressBar('  playing [:bar] :percent :etas', {
-      complete: '=',
+function isURL(url) {
+  return (!!~url.indexOf('http://') || !!~url.indexOf('https://'));
+}
+
+function displayData(stream, data, cb) {
+  var len, bar;
+
+  if (stream.headers) {
+    len = parseInt(stream.headers['content-length'], 10);
+    bar = new ProgressBar('  playing [:bar] :percent :etas', {
+      complete: '#',
       incomplete: ' ',
-      width: 20,
+      width: 50,
       total: len
     });
 
-    res.on('end', function () {
-      console.log('\n');
-    });
+  }
 
-    res.on('data', function(chunk) {
-      bar.tick(chunk.length);
-    });
+  console.log('\n');
+
+  console.log(stream);
+  stream.on('end', function () {
+    if (cb) {
+      cb();
+    } else console.log('\n');
+  });
+
+  stream.on('data', function(chunk) {
+    if (bar) bar.tick(chunk.length);
+  });
+}
+
+function playLocal(path) {
+  fs.lstat(path, function(err, stats) {
+    if (stats.isDirectory()) {
+      playDirectory(path);
+    } else if (stats.isFile()) {
+      playFile(path);
+    } else {
+      console.log('This doesn\'t appear to be a file, directory, or URL: ', path);
+    }
+  });
+}
+
+function playAllFiles(files, idx, cb) {
+  if (idx > files.length) {
+    if (cb) cb();
+    return;
+  }
+  playFile(files[idx], function() {
+    playAllFiles(files, idx + 1);
+  });
+}
+
+function playDirectory(path, cb) {
+  var files = [];
+
+  ls(path)
+  .on('data', function(data) {
+    files.push(data.path);
+  }).on('end', function() {
+    playAllFiles(files, 0, cb);
+  });
+}
+
+function playFile(path, cb) {
+  console.log(path);
+  fs.createReadStream(path)
+  .on('data', function(stream) {
+    // displayData(stream, null, cb);
+  })
+  .on('end', function() {
+    if (cb) cb();
   })
   .pipe(new lame.Decoder())
   .pipe(speaker);
